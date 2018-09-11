df5389b52fb156ea6eee3401cf929f59f613d5a4
Steve Luxford <steve.luxford@gmail.com>
2018-09-11 15:42:02 +1000

    update10

--- tasks/opsmanager-logout/task1.sh
+++ tasks/opsmanager-logout/task1.sh
@@ -14,7 +14,7 @@ PKS_GUID=$(echo "$PRODUCTS" | jq -r '.[] | .guid' | grep nr-firehose-nozzle)
 om-linux -t https://${OPSMAN_HOST} -c ${OPSMAN_CLIENT_ID} -s ${OPSMAN_CLIENT_SECRET} -k curl --path /api/v0/stemcell_assignments -x PATCH -d '{
     "products": [
     {
-    "guid":"PKS_GUID",
+    "guid":"#{PKS_GUID}",
     "staged_stemcell_version": "3586.40"
     }
     ]
